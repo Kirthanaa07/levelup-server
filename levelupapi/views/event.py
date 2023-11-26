e32fdc64ebab47a8c9d66eb72c393cacf8b88795
Kirthana Sukumaran <kirthanaa07@gmail.com>
2023-11-25 23:41:33 -0600

    first commit

---
+++ levelupapi/views/event.py
@@ -0,0 +1,86 @@
+"""View module for handling requests about game types"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from levelupapi.models import Event
+from levelupapi.models import Game
+from levelupapi.models import Gamer
+
+class EventView(ViewSet):
+    """Level up game view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single game type
+        Returns:
+            Response -- JSON serialized game type
+        """
+
+        try:
+          event = Event.objects.get(pk=pk)
+          serializer = EventSerializer(event)
+          return Response(serializer.data)
+        except Event.DoesNotExist as ex:
+          return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+
+    def list(self, request):
+        """Handle GET requests to get all game types
+        Returns:
+            Response -- JSON serialized list of game types
+        """
+        event = Event.objects.all()
+        serializer = EventSerializer(event, many=True)
+        return Response(serializer.data)
+    
+    def create(self, request):
+        """Handle POST operations
+        Returns
+            Response -- JSON serialized event instance
+        """
+        game = Game.objects.get(pk=request.data["game"])
+        gamer = Gamer.objects.get(pk=request.data["organizer"])
+
+        event = Event.objects.create(
+            description=request.data["description"],
+            date=request.data["date"],
+            time=request.data["time"],
+            game=game,
+            organizer=gamer,
+        )
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+    
+    def update(self, request, pk):
+        """Handle PUT requests for an event
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+
+        event = Event.objects.get(pk=pk)
+        event.description = request.data["description"]
+        event.date = request.data["date"]
+        event.time = request.data["time"]
+
+        game = Game.objects.get(pk=request.data["game"])
+        event.game = game
+
+        gamer = Gamer.objects.get(pk=request.data["organizer"])
+        event.organizer = gamer
+
+        event.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk):
+        event = Event.objects.get(pk=pk)
+        event.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+class EventSerializer(serializers.ModelSerializer):
+    """JSON serializer for game types
+    """
+    class Meta:
+        model = Event
+        fields = ('id', 'game', 'description', 'date', 'time', 'organizer')
+        depth = 0
